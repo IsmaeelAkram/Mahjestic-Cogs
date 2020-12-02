9c849ae8ec10894dcd4d6192f06c0ae49c4dc734
Ismaeel Akram <ismaeelakram01@gmail.com>
2020-12-02 09:12:56 -0500

    Set up modmail

--- modmail/__init__.py
+++ modmail/__init__.py
@@ -1,4 +1,4 @@
 from .modmail import Modmail
 
 def setup(bot):
-    bot.add_cog(Modmail())
+    bot.add_cog(Modmail(bot))
--- modmail/modmail.py
+++ modmail/modmail.py
@@ -1,11 +1,13 @@
-from redbot.core import Config, commands
+from redbot.core import commands
+from redbot.core.bot import Config, Red
 import discord
 from discord.ext import commands as Commands
 
 class Modmail(commands.Cog):
     """Simple modmail cog"""
 
-    @commands.Cog.listener()
-    async def on_message(self, ctx: Commands.Context):
-        if not ctx.message.guild:
-            await ctx.channel.send("ModMail is coming soon to this bot.")
+    def __init__(self, bot: Red):
+        self.bot = bot
+        self.config = Config.get_conf(self, identifier=2480948239048209)
+
+   
