5f48c8288216d89eee4363b2da34ab35b472029f
Ismaeel Akram <ismaeelakram01@gmail.com>
2020-12-01 19:33:48 -0500

    Add info

--- modmail/modmail.py
+++ modmail/modmail.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands as Commands
 
 class Modmail(commands.Cog):
-    """Simple modmail cog for Mahjestic Manor"""
+    """Simple modmail cog"""
 
     @commands.Cog.listener()
     async def on_message(self, ctx: Commands.Context):
