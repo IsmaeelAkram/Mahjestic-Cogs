f0140c6aef2b5bef069cb59f76db058d05876541
Ismaeel Akram <ismaeelakram01@gmail.com>
2021-02-23 21:32:27 -0500

    Fix bug where external emojis don’t load

--- stealemoji/stealemoji.py
+++ stealemoji/stealemoji.py
@@ -3,6 +3,8 @@ from redbot.core.bot import Config, Red
 import discord
 import logging
 import re
+import random
+import string
 
 class StealEmoji(commands.Cog):
     def __init__(self, bot: Red):
@@ -10,8 +12,13 @@ class StealEmoji(commands.Cog):
         self.config = Config.get_conf(self, identifier=2430948239048209)
         self.logger = logging.getLogger('red.mahjesticcogs.stealemoji')
 
-    async def steal_emoji(self, emoji: discord.Emoji, msg: discord.Message):
-        await msg.channel.send(embed=discord.Embed(title="Emoji stolen!", description="I just stole this emoji!").set_thumbnail(url=emoji.url))
+    async def steal_emoji(self, emoji_name: str, emoji_id: int, msg: discord.Message, guild: discord.Guild):
+        emoji_url = "https://cdn.discordapp.com/emojis/<emoji_id>.png".replace("<emoji_id>", emoji_id)
+        await msg.channel.send(embed=discord.Embed(title="Emoji stolen!", description="I just stole this emoji!").set_thumbnail(url=emoji_url))
+
+        new_name = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+        await guild.create_custom_emoji(name=(new_name), image=emoji_url)
+        
 
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
@@ -22,12 +29,11 @@ class StealEmoji(commands.Cog):
         emojis = []
         for e in emojis_text:
             id = e.split(':')[2].replace('>', '')
+            name = None
             emoji = self.bot.get_emoji(int(id))
-            emojis.append(emoji)
+            emojis.append((name, id))
 
-            self.logger.info(emoji)
-            self.logger.info(msg.guild)
-            if emoji.guild.id != msg.guild.id:
-                await self.steal_emoji(emoji, msg)
-
-        
+        for emoji in emojis:
+            guild = self.bot.get_guild(msg.guild.id)
+            if guild != msg.guild.id:
+                await self.steal_emoji(emoji[0], emoji[1], msg, guild)
